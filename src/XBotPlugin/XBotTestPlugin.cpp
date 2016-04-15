775f7cbe4443adf0b9bbd3436a0feb41de743865
Luca Muratore <luca.muratore@iit.it>
2016-04-15 19:30:36 +0200

    Test plugin cleanup

--- src/XBotPlugin/XBotTestPlugin.cpp
+++ src/XBotPlugin/XBotTestPlugin.cpp
@@ -12,13 +12,27 @@ XBot::XBotTestPlugin::XBotTestPlugin(std::string name,
 bool XBot::XBotTestPlugin::init(void)
 {
     DPRINTF("XBotTestPlugin init()\n");
-    l_arm_pos_ref[123] = 0;
+    std::map<int, float> l_arm_link_pos;
+    chain->get_chain_link_pos("left_arm", l_arm_link_pos);
+    for( auto& j : l_arm_link_pos) {
+        DPRINTF("l_arm_link_pos Joint : %d - link pos : %f\n", j.first, j.second);
+        l_arm_pos_ref[j.first] = j.second;
+    }
+    DPRINTF("27_link_pos : %f\n", l_arm_link_pos.at(27));
+    l_arm_pos_ref[27] = l_arm_link_pos.at(27) + 0.05;
     
     return true;
 }
 
 void XBot::XBotTestPlugin::run(void)
-{
+{  
+    std::map<int, uint16_t> l_hand_j_rtt;
+    chain->get_chain_rtt("left_hand", l_hand_j_rtt);
+
+    for( auto& j : l_hand_j_rtt) {
+//         DPRINTF("left_hand Joint : %d - RTT : %d\n", j.first, j.second); // NOTE avoid printing std::string with XENOMAI printf 
+    }
+    
     std::map<std::string, uint16_t> l_arm_rtt;
     chain->get_chain_rtt("left_arm", l_arm_rtt);
 
@@ -26,55 +40,29 @@ void XBot::XBotTestPlugin::run(void)
         std::string j_name = j.first;
         int rtt = j.second;
         int rid = model->joint2Rid(j_name);
-        //DPRINTF("left_arm Joint : %d - RTT : %d\n", rid, rtt); // NOTE avoid printing std::string with XENOMAI printf 
+//         DPRINTF("left_arm Joint : %d - RTT : %d\n", rid, rtt); // NOTE avoid printing std::string with XENOMAI printf 
     }
-
     
-//     std::map<int, uint16_t> l_hand_j_rtt;
-//     chain->get_chain_rtt("left_hand", l_hand_j_rtt);
-// 
-//     for( auto& j : l_hand_j_rtt) {
-//         DPRINTF("left_hand Joint : %d - RTT : %d\n", j.first, j.second); // NOTE avoid printing std::string with XENOMAI printf 
-//     }
-//     
-//     std::map<std::string, uint16_t> l_arm_temperature;
-//     chain->get_chain_max_temperature("left_arm", l_arm_temperature);
-// 
-//     for( auto& j : l_arm_temperature) {
+    std::map<std::string, uint16_t> l_arm_temperature;
+    chain->get_chain_max_temperature("left_arm", l_arm_temperature);
+
+    for( auto& j : l_arm_temperature) {
 //         DPRINTF("Joint : %d - TEMPERATURE : %d\n",  model->joint2Rid(j.first), j.second); // NOTE avoid printing std::string with XENOMAI printf 
-//     }
-//     
-//     std::map<std::string, int16_t> l_arm_torque;
-//     chain->get_chain_torque("left_arm", l_arm_torque);
-// 
-//     for( auto& j : l_arm_torque) {
-//         DPRINTF("Joint : %d - TORQUE : %d\n", model->joint2Rid(j.first), j.second); // NOTE avoid printing std::string with XENOMAI printf 
-//     }
+    }
     
-    
-    
-    l_arm_pos_ref[123] += 0.1;
-    
-    chain->set_chain_pos_ref("left_arm", l_arm_pos_ref);
-    
-    std::map<int, int16_t> l_arm_vel_ref;
-    l_arm_vel_ref[123] = 4;
-    
-    chain->set_chain_vel_ref("left_arm", l_arm_vel_ref);
-    
-    std::map<int, std::vector<uint16_t>> l_arm_gains;
-    std::vector<uint16_t> g(5);
-    g[0] = 210;
-    g[1] = 211;
-    g[2] = 212;
-    g[3] = 213;
-    g[4] = 214;
-    l_arm_gains[123] = g;
-    
-    chain->set_chain_gains("left_arm", l_arm_gains);
+    std::map<std::string, int16_t> l_arm_torque;
+    chain->get_chain_torque("left_arm", l_arm_torque);
 
+    for( auto& j : l_arm_torque) {
+//         DPRINTF("Joint : %d - TORQUE : %d\n", model->joint2Rid(j.first), j.second); // NOTE avoid printing std::string with XENOMAI printf 
+    }
     
     
+
+    // NOTE move commented out
+    chain->set_chain_pos_ref("left_arm", l_arm_pos_ref);
+
+   
 }
 
 bool XBot::XBotTestPlugin::close(void)
