83f32df16132ff8482e647172577338001b785cb
Luca Muratore <luca.muratore@iit.it>
2016-04-18 15:09:53 +0200

    IXbotRobt empty skeleton

---
+++ include/XBotCore/IXBotRobot.h
@@ -0,0 +1,39 @@
+/*
+   Copyright (C) 2016 Italian Institute of Technology
+
+   Developer:
+       Luca Muratore (2016-, luca.muratore@iit.it)
+   
+*/
+
+/**
+ *
+ * @author Luca Muratore (2016-, luca.muratore@iit.it)
+*/
+
+#ifndef __I_X_BOT_ROBOT_H__
+#define __I_X_BOT_ROBOT_H__
+
+#include <map>
+#include <string>
+
+namespace XBot
+{
+    class IXBotRobot;
+}
+
+/**
+ * @brief TBD
+ * 
+ */
+class XBot::IXBotRobot
+{
+    
+public:   
+
+
+
+
+};
+
+#endif //__I_X_BOT_ROBOT_H__
