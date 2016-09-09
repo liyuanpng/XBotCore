2f162eb7ecb691ead996c4865cb3875aaf61f750
Luca Muratore <luca.muratore@iit.it>
2016-09-09 14:09:31 +0200

    License updated.

--- include/XBotCore/IXBotCommunication.h
+++ include/XBotCore/IXBotCommunication.h
@@ -3,6 +3,19 @@
 
    Developer:
        Luca Muratore (2016-, luca.muratore@iit.it)
+       
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>       
    
 */
 
@@ -10,6 +23,7 @@
  *
  * @author Luca Muratore (2016-, luca.muratore@iit.it)
 */
+
 
 #ifndef __I_X_BOT_COMMUNICATION_H__
 #define __I_X_BOT_COMMUNICATION_H__
