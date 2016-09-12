ed6fcb541938bb828d3da29ada28692dae04fb2a
Luca Muratore <luca.muratore@iit.it>
2016-09-12 09:27:10 +0200

    Added missing license.

--- src/XBotCommunicationHandler.cpp
+++ src/XBotCommunicationHandler.cpp
@@ -1,3 +1,29 @@
+/*
+   Copyright (C) 2016 Italian Institute of Technology
+
+   Developer:
+       Luca Muratore (2016-, luca.muratore@iit.it)
+       
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>       
+   
+*/
+
+/**
+ *
+ * @author Luca Muratore (2016-, luca.muratore@iit.it)
+*/
+
 #include <XBotCore/XBotCommunicationHandler.h>
 
 #include <sys/stat.h>
