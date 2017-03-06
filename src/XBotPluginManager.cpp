08a45f7fc02567a92ffd14500a41c742f90ef673
Luca Muratore <luca.muratore@iit.it>
2017-03-06 21:21:45 +0100

    Removed YARP since not already implemented. Added XBotCommunicationHandler.

--- src/XBotPluginManager.cpp
+++ src/XBotPluginManager.cpp
@@ -18,7 +18,7 @@
 */
 
 #include <XCM/XBotCommunicationInterface.h>
-// #include <XBotCore-interfaces/XBotCommunicationHandler.h>
+#include <XCM/XBotCommunicationHandler.h>
 #include <XBotCore-interfaces/XDomainCommunication.h>
 #include <csignal>
 
