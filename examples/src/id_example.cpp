3c774c313bdc24a272bd851ce99c2e1bf321d0da
Arturo Laurenzi <arturo.laurenzi@iit.it>
2017-04-11 17:27:15 +0200

    Added some comments to ID example

--- examples/src/id_example.cpp
+++ examples/src/id_example.cpp
@@ -45,24 +45,31 @@ void XBot::IdExample::control_loop(double time, double period)
     static double PERIOD = 1.0;
     static double OMEGA = 2.0 * 3.1415 / PERIOD;
 
-    if(command.read(current_command)){
+    /* If "damp" command is received, start decreasing OMEGA so as to come to a stop */
+    if(command.read(current_command) && current_command.str() == "damp"){
         damp_motion = true;
         std::cout << "Damping motion!" << std::endl;
     }
 
-    if( damp_motion ) OMEGA *= 0.995;
+    if( damp_motion ) OMEGA *= 0.999;
 
+    /* Compute reference trajectory up to 2nd order derivatives */
     _qref = _q0 + _dq * std::sin(OMEGA*(time-_first_loop_time));
     _qdotref = _dq * std::cos(OMEGA*(time-_first_loop_time)) * OMEGA;
     _qddotref = -1.0 * _dq * std::sin(OMEGA*(time-_first_loop_time)) * OMEGA * OMEGA;
 
-    _model->syncFrom(*_robot, Sync::Position, Sync::Velocity);
-    _model->setJointAcceleration(_qddotref); // FIXME syncFrom does the update, but now we need another one!!!!
+    /* Set actual robot positions / velocities to the model used for ID */
+    _model->syncFrom_no_update(*_robot, Sync::Position, Sync::Velocity);
+
+    /* Set acceleration reference */
+    _model->setJointAcceleration(_qddotref);
     _model->update();
 
+    /* Set tau = B*qddotref + h(q, qdot) */
     _model->computeInverseDynamics(_tau);
-    _model->getInertiaMatrix(_B);
 
+    /* Add the PD term */
+    _model->getInertiaMatrix(_B);
     _robot->model().getJointPosition(_q);
     _robot->model().getJointVelocity(_qdot);
 
@@ -70,6 +77,7 @@ void XBot::IdExample::control_loop(double time, double period)
 
     _tau += _B*( kp*(_qref-_q) + kd*(_qdotref-_qdot) );
 
+    
     _model->setJointEffort(_tau);
     _robot->setPositionReference(_qref);
     _robot->setReferenceFrom(*_model, Sync::Effort);
