eb9f6549a847251dd6d6eccb0cb8c41f3b396357
pepperig <giusepperig@hotmail.it>
2017-11-10 11:25:40 +0100

    fix wrong include

--- src/Loader.cpp
+++ src/Loader.cpp
@@ -17,7 +17,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>
 */
 
-#include <XBotCore/Loader.h>
+#include <XCM/Loader.h>
 #include <boost/bind.hpp>
 #include <string>
 #include <atomic>
